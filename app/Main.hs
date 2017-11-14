eeef27185d62c5284b536b863b0cd4a61bc001fb
jmorte14 <jmorte14@student.aau.dk>
2017-11-14 14:25:48 +0100

    work

--- app/Main.hs
+++ app/Main.hs
@@ -1,69 +1,79 @@
--- module Main where
+module Main where
 
--- main :: IO String
--- main = getLine
-
-one = 1
+main :: IO String
+main = getLine
 
 type K = Integer
 type C = Rational
 type X = String
-data P = CONST C | VAR X | PADD P P | PMUL C P | PPOW X K deriving (Eq, Show)
-data E = POL P | EADD E E | EMUL E E | DIV E E | D E | SIN E | COS E | EPOW E K deriving (Eq, Show)
+data P = CONST C | VAR X | PADD P P | PMUL C P | POW X K deriving (Eq, Show)
+data E = POL P | EADD E E | EMUL E E | DIV E E | D X E | SIN E | COS E deriving (Eq, Show)
 
 -- -- Reduction
 -- (3)
-reduce (EMUL (POL (PPOW e1 a)) (POL (PPOW e2 b))) = if e1 == e2
-                                                  then POL (PPOW e1 (a+b))
-                                                  else EMUL (POL (PPOW e1 a)) (POL (PPOW e2 b))
-
+reduce (EMUL (POL (POW e1 a)) (POL (POW e2 b))) =
+            if e1 == e2
+            then POL (POW e1 (a+b))
+            else EMUL (POL (POW e1 a)) (POL (POW e2 b))
 -- (4)
-reduce (DIV (POL (PPOW e1 a)) (POL (PPOW e2 b))) = if e1 == e2
-                                                 then POL (PPOW e1 (a-b))
-                                                 else DIV (POL (PPOW e1 a)) (POL (PPOW e2 b))
-
+reduce (DIV (POL (POW e1 a)) (POL (POW e2 b))) =
+            if e1 == e2
+            then POL (POW e1 (a-b))
+            else DIV (POL (POW e1 a)) (POL (POW e2 b))
 -- (5)
-reduce (EADD (EMUL (SIN (POL (VAR e1))) (SIN (POL (VAR e2)))) (EMUL (COS (POL (VAR e3))) (COS (POL (VAR e4))))) = if e1 == e2 && e2 == e3 && e3 == e4
-                                                                                                                  then POL (PPOW e1 0)
-                                                                                                                  else EADD (EMUL (SIN (POL (VAR e1))) (SIN (POL (VAR e2)))) (EMUL (COS (POL (VAR e3))) (COS (POL (VAR e4))))
+reduce (EADD (EMUL (SIN (POL (VAR e1))) (SIN (POL (VAR e2)))) (EMUL (COS (POL (VAR e3))) (COS (POL (VAR e4))))) =
+            if e1 == e2 && e2 == e3 && e3 == e4
+            then POL (POW e1 0)
+            else EADD (EMUL (SIN (POL (VAR e1))) (SIN (POL (VAR e2)))) (EMUL (COS (POL (VAR e3))) (COS (POL (VAR e4))))
 
+reduce (EADD (EMUL (COS (POL (VAR e1))) (COS (POL (VAR e2)))) (EMUL (SIN (POL (VAR e3))) (SIN (POL (VAR e4))))) =
+            if e1 == e2 && e2 == e3 && e3 == e4
+            then POL (POW e1 0)
+            else EADD (EMUL (COS (POL (VAR e1))) (COS (POL (VAR e2)))) (EMUL (SIN (POL (VAR e3))) (SIN (POL (VAR e4))))
 -- (6)
-reduce (EADD e1 e2) = EADD (reduce e2) (reduce e1)
-
+reduce (EADD e1 e2) = EADD (reduce e1) (reduce e2)
 -- (7, 9)
--- reduce (EMUL e1 e2) = let a = reduce e1
---                           b = reduce e2
---                       in if a == b && isPOL a
---                          then POL (getPOL a)
---                          else EMUL b a
-
+reduce (EMUL e1 e2) =
+            if e1 == e2 && isVAR e1
+            then POL (POW (getVAR e1) 2)
+            else EMUL (reduce e1) (reduce e2)
 -- (8)
-reduce (EADD (EADD e1 e2) e3) = EADD e1 (EADD e2 e3)
-
+reduce (EADD (EADD e1 e2) e3) = EADD (reduce (EADD e1 e2)) (reduce e3)
 -- (10)
-reduce (EMUL (EMUL e1 e2) e3) | reduce e1 == reduce e2 = EMUL (reduce (EMUL (reduce e1) (reduce e2))) (reduce e3) -- && isPOL a
-                              | reduce e2 == reduce e3 = EMUL (reduce e1) (reduce (EMUL (reduce e2) (reduce e3)))
-                              | otherwise = EMUL (reduce e1) (EMUL (reduce e2) (reduce e3))
-
+reduce (EMUL (EMUL e1 e2) e3)
+            | e1 == e2 = EMUL (EMUL e1 e2) e3
+            | e2 == e3 = EMUL e1 (EMUL e2 e3)
+            | otherwise = EMUL e1 (EMUL e2 e3)
 -- (11)
-reduce (EMUL e1 (EADD e2 e3)) = let a = reduce e1
-                                    b = reduce e2
-                                    c = reduce e3
-                                in EADD (EMUL a b) (EMUL a c)
-
+reduce (EMUL e1 (EADD e2 e3)) = EADD (EMUL e1 e2) (EMUL e1 e3)
 -- (12)
-reduce (EMUL e1 e2) | e1 == POL (CONST one) = reduce e2
-                    | e2 == POL (CONST one) = reduce e1
-                    | otherwise = EMUL (reduce e1) (reduce e2)
+reduce (POL (PMUL c e)) =
+            if c == 1
+            then POL e
+            else POL (PMUL c e)
+-- other reductions
+reduce (DIV e1 e2) = DIV (reduce e1) (reduce e2)
+-- reduce (D x e1) =
+reduce (SIN e1) = SIN (reduce e1)
+reduce (COS e1) = COS (reduce e1)
+reduce (POL p) = POL (pReduce p)
+pReduce (VAR x) = VAR x
+pReduce (PMUL c p) = PMUL c (pReduce p)
+pReduce (PADD p1 p2) = PADD (pReduce p1) (pReduce p2)
+pReduce (POW x k) = POW x k
+pReduce (CONST c) = CONST c
 
--- Helpers
--- getPOL (POL p1) = p1
+-- Helper functions
+isVAR (POL (VAR x)) = True
+isVAR _ = False
 
--- isPOL (POL p1) = True
--- isPOL _ = False
+getVAR (POL (VAR x)) = x
 
-add (VAR e1) (VAR e2) = PADD (VAR e1) (VAR e2)
-mul = "not implemented"
-mulScal = "not implemented"
-div = "not implemented"
-diff = "not implemented"
+-- User functions
+t1 = reduce (DIV (EMUL (EMUL (POL (CONST 1))
+                             (EADD (EMUL (SIN (POL (VAR "x")))
+                                         (SIN (POL (VAR "x"))))
+                                   (EMUL (COS (POL (VAR "x")))
+                                         (COS (POL (VAR "x"))))))
+                       (POL (POW "x" 3)))
+                 (POL (POW "x" 2)))
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,11 +4,3 @@ module Lib
 
 someFunc :: IO String
 someFunc = getLine
-
-type K = Integer
-type C = Rational
-type X = String
-data P = VAR X | PADD P P | PMUL C P | POW X K deriving Show
-data E = POL P | EADD E E | EMUL E E | EDIV E E | D E | SIN E | COS E deriving Show
-
-add (VAR e1) (VAR e2) = PADD (VAR e1) (VAR e2)
