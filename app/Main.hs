a25d7031e3d049321ce8a0a210a32f79a5840f3d
jmorte14 <jmorte14@student.aau.dk>
2017-11-16 21:25:18 +0100

    more diff

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ type K = Integer
 type C = Rational
 type X = String
 data P = VAR X | PADD P P | PMUL C P | POW X K deriving (Eq, Show)
-data E = POL P | EADD E E | EMUL E E | DIV E E | D X E | SIN E | COS E deriving (Eq, Show)
+data E = POL P | EADD E E | EMUL E E | EPOW E K | DIV E E | D X E | SIN E | COS E deriving (Eq, Show)
 
 -- -- Reduction
 startReducing e =
@@ -36,6 +36,14 @@ reduce (EADD (EMUL (COS (POL (VAR e1))) (COS (POL (VAR e2)))) (EMUL (SIN (POL (V
             else EADD (EMUL (COS (POL (VAR e1))) (COS (POL (VAR e2)))) (EMUL (SIN (POL (VAR e3))) (SIN (POL (VAR e4))))
 -- (6)
 reduce (EADD e1 e2) = EADD (reduce e1) (reduce e2)
+-- (12)
+reduce (EMUL e one) = e
+
+reduce (EMUL one e) = e
+
+reduce (EMUL e zero) = zero
+
+reduce (EMUL zero e) = zero
 -- (7, 9)
 reduce (EMUL e1 e2) =
             if e1 == e2 && isVAR e1
@@ -53,22 +61,22 @@ reduce (EMUL e1 (EMUL e2 e3)) = EMUL (reduce e1) (reduce (EMUL e2 e3))
 reduce (EMUL (EADD e1 e2) e3) = EADD (EMUL e1 e3) (EMUL e1 e3)
 
 reduce (EMUL e1 (EADD e2 e3)) = EADD (EMUL e1 e2) (EMUL e1 e3)
--- (12)
-reduce (EMUL e (POL (POW x 0))) = e
-
-reduce (EMUL (POL (POW x 0)) e) = e
 -- other reductions
 reduce (DIV e1 e2) = DIV (reduce e1) (reduce e2)
 reduce (D x e)
-            | isCOS e = neg (SIN (getCOS e))
-            | isSIN e = COS (getSIN e)
-            | isNEG e && isCOS (getNEG e) = SIN (getCOS (getNEG e))
-            | isNEG e && isSIN (getNEG e) = neg (COS (getSIN (getNEG e)))
+            | isVAR e = if getVAR e == x
+                        then one
+                        else zero
+--            | isEADD e = EADD (D x (getEADDe1 e)) (D x (getEADDe2 e))
+--            | isPADD e =
+            | isPMUL e = EMUL (num (getPMULc e)) (D x (POL (getPMULp e)))
+            | isEMUL e = EADD (EMUL (D x (getEMULe1 e)) (getEMULe2 e)) (EMUL (getEMULe1 e) (D x (getEMULe2 e)))
+            | isDIV e = DIV (EADD (EMUL (D x (getDIVe1 e)) (getDIVe2 e)) (neg (EMUL (getDIVe1 e) (D x (getDIVe2 e))))) (EPOW (getDIVe2 e) 2)
             | isPOW e = POL (PMUL (fromInteger (getPOWk e)) (POW (getPOWx e) ((getPOWk e) - 1)))
-            | isPMUL e =
-            | isEMUL e = EMUL (reduce (D x (getEMULe1 e))) (reduce (D x (getEMULe2 e)))
-            | isPADD e =
-            | isEADD e = EADD (reduce (D x (getEADDe1 e))) (reduce (D x (getEADDe2 e)))
+--            | isCOS e = neg (SIN (getCOS e))
+--            | isSIN e = COS (getSIN e)
+--            | isNEG e && isCOS (getNEG e) = SIN (getCOS (getNEG e))
+--            | isNEG e && isSIN (getNEG e) = neg (COS (getSIN (getNEG e)))
 reduce (SIN e) = SIN (reduce e)
 reduce (COS e) = COS (reduce e)
 reduce (POL p) = POL (pReduce p)
@@ -88,6 +96,26 @@ isVAR (POL (VAR x)) = True
 isVAR _ = False
 getVAR (POL (VAR x)) = x
 
+isPMUL (POL (PMUL c p)) = True
+isPMUL _ = False
+getPMULp (POL (PMUL c p)) = p
+getPMULc (POL (PMUL c p)) = c
+
+isEMUL (EMUL e1 e2) = True
+isEMUL _ = False
+getEMULe1 (EMUL e1 e2) = e1
+getEMULe2 (EMUL e1 e2) = e2
+
+isPOW (POL (POW x k)) = True
+isPOW _ = False
+getPOWx (POL (POW x k)) = x
+getPOWk (POL (POW x k)) = k
+
+isDIV (DIV e1 e2) = True
+isDIV _ = False
+getDIVe1 (DIV e1 e2) = e1
+getDIVe2 (DIV e1 e2) = e2
+
 isCOS (COS e) = True
 isCOS _ = False
 getCOS (COS e) = e
@@ -95,11 +123,6 @@ getCOS (COS e) = e
 isSIN (SIN e) = True
 isSIN _ = False
 getSIN (SIN e) = e
-
-isPOW (POL (POW x k)) = True
-isPOW _ = False
-getPOWx (POL (POW x k)) = x
-getPOWk (POL (POW x k)) = k
 
 isNEG (EMUL (POL (PMUL c (POW x 0))) e) =
             if c < 0
@@ -109,6 +132,10 @@ isNEG _ = False
 getNEG (EMUL (POL (PMUL c (POW x 0))) e) = e
 
 neg e = EMUL (POL (PMUL (-1) (POW "x" 0))) e
+
+num e = POL (PMUL e (POW "x" 0))
+one = POL (POW "x" 0)
+zero = POL (PMUL 0 (POW "x" 0))
 -- User functions
 t1 = startReducing (DIV (EMUL (EMUL (POL  (POW "x" 0))
                                     (EADD (EMUL (SIN (POL (VAR "x")))
@@ -117,3 +144,6 @@ t1 = startReducing (DIV (EMUL (EMUL (POL  (POW "x" 0))
                                                 (COS (POL (VAR "x"))))))
                               (POL  (POW "x" 3)))
                         (POL  (POW "x" 2)))
+
+
+t2 = EMUL (POL (POW "x" 0)) (EMUL (POL (PMUL 2 (POW "x" 0))) (POL (POW "x" 0)))
